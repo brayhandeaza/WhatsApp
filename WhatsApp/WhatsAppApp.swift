d51c90a412695f810ca78d8d8de2366af4e62aea
Brayhan De Aza <brayhandeaza@gmail.com>
2020-12-12 13:22:48 -0500

    Initial Commit

---
+++ WhatsApp/WhatsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsAppApp.swift
+//  WhatsApp
+//
+//  Created by Brayhan De Aza on 12/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WhatsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
